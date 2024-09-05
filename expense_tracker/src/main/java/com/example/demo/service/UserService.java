8ff4cbe439075ecaf945dc042b4886e8ca652d98
richard5zx <richardchao2009@gmail.com>
2024-09-05 11:22:44 +0800

    initialize interface and files for UserService implementation

---
+++ expense_tracker/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+public interface UserService {
+	// C
+	
+	// R
+	
+	// U
+	
+	// D
+}
---
+++ expense_tracker/src/test/java/com/example/demo/service/UserServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.service.impl.UserServiceImpl;
+
+@SpringBootTest	
+public class UserServiceImplTest {
+	@Autowired
+	UserServiceImpl usi;
+	
+	
+}
