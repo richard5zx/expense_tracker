400dbb1a4b63add5e05048c88f7121b1de84a887
richard5zx <richardchao2009@gmail.com>
2024-09-13 16:38:39 +0800

    initial draft for expensemapper.java

--- expense_tracker/src/main/java/com/example/demo/dao/ExpenseMapper.java
+++ expense_tracker/src/main/java/com/example/demo/dao/ExpenseMapper.java
@@ -1,8 +1,40 @@
 package com.example.demo.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.example.demo.model.Expense;
 
 @Mapper
-public class ExpenseMapper {
-
+public interface ExpenseMapper {
+	// C
+	@Insert("insert into user(user_id,category,expense,currency)"
+			+ " values(#{user_id},#{category},#{expense},#{currency})")
+	public void addExpense(Expense expense);
+	
+	// R
+	@Select("select * from expense")
+	public List<Expense> selectAll();
+	
+	@Select("select * from expense where category=#{category}")
+	public List<Expense> selectByCategory(String category);
+	
+	@Select("select * from expense where currency=#{currency}")
+	public List<Expense> selectByCurrency(String currency);
+	
+	@Select("select * from expense where timestamp between #{min} and #{max}")
+	public List<Expense> selectByTime(String min, String max);
+	
+	// U
+	@Update("update expense set category=#{category},expense=#{expense},category=#{category}")
+	public void updateExpense(Expense expense);
+	
+	// D
+	@Delete("delete from user where user_id=#{expense_id}")
+	public void deleteExpense(int expense_id);
 }
